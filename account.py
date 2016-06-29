84911eaec7d1534012c7e20f0eed3b9c27f6b96c
morrillo <gustavo.orrillo@moldeo.coop>
2016-06-29 09:05:26 -0300

    Fixed bug

--- account.py
+++ account.py
@@ -5,4 +5,4 @@ from openerp import models, api, fields
 class account_bank_statement_line(models.Model):
 	_inherit = "account.bank.statement.line"
 
-	check_id = fields.Many2one('account.check',domain=[('state','in',[('holding')])
+	check_id = fields.Many2one('account.check',domain=[('state','in',[('holding')])])
