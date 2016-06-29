45c4869c437eeeaea36ab0789cb52e0e28d77365
morrillo <gustavo.orrillo@moldeo.coop>
2016-06-29 09:25:19 -0300

    Added onchange

--- account.py
+++ account.py
@@ -6,3 +6,12 @@ class account_bank_statement_line(models.Model):
 	_inherit = "account.bank.statement.line"
 
 	check_id = fields.Many2one('account.check',domain=[('state','in',[('holding')])])
+
+	@api.onchange('check_id')
+	def change_check(self):
+		if self.check_id.source_partner_id:
+			self.partner_id = self.check_id.source_partner_id.id
+		if self.check_id.amount:
+			self.amount = self.check_id.amount
+
+
